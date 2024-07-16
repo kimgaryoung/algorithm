67669e71745ee1b9e2dcff391511d992f55b67de
kgy <kgy@DESKTOP-3RV7MJ8>
2024-07-16 13:47:19 +0900

    Add project files.

---
+++ codeup.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int a, r, n;
+    cin >> a;
+    cin >> r;
+    cin >> n;
+
+    vector<unsigned int> mm = { 0 };
+    mm.push_back(a);
+
+    for (int i = 1; i < n; ++i) {
+        unsigned int k = mm.at(i);
+        
+
+        mm.push_back(k);
+    }
+
+    cout << mm.at(n) << endl;
+
+    return 0;
+}
